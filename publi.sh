8f7d1cf6562512640fb4637e80c3666784bb920d
XMDPLUS <dev@xmd.co.kr>
2016-06-20 22:27:38 +0900

    2016년 6월 20일 월요일 22시 27분 38초 KST

--- publi.sh
+++ publi.sh
@@ -5,6 +5,5 @@ rm -rf _site/
 jekyll build
 git add --all
 git commit -m "`date`"
-git remote add origin https://xmdplus@github.com/XMDPLUS/xmdplus.github.io.git
 git push origin master
 git subtree push --prefix  _site/ origin gh-pages

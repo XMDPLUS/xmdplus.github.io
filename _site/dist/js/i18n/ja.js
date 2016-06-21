4cae1f3dc401c655bb70d0824cb6d80dd4b2ffcf
syjeonxmd <syjeon.xmd@gmail.com>
2016-06-21 11:01:16 +0900

    update content

--- _site/dist/js/i18n/ja.js
+++
@@ -1,4 +0,0 @@
-var messages = {
-    "has_results" : "キーワード[{TERM}]の検索結果は[{CNT}]件です。",
-    "no_result" : "キーワード[[{TERM}]の検索結果がありません。"
-};
--- _site/ja/publi.sh
+++
@@ -1,9 +0,0 @@
-# publi.sh
-# change the branch names appropriately
-git checkout master
-rm -rf _site/
-jekyll build
-git add --all
-git commit -m "`date`"
-git push origin master
-git subtree push --prefix  _site/ origin gh-pages

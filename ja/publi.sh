8ba8ce6377aa5c3dcf66dedb359d109dfdd5885f
XMDPLUS <dev@xmd.co.kr>
2016-06-21 14:03:37 +0900

    2016년 6월 21일 화요일 14시 03분 37초 KST

--- ja/publi.sh
+++ ja/publi.sh
@@ -1,9 +1,14 @@
 # publi.sh
 # change the branch names appropriately
 git checkout xphelp
-rm -rf _site/
+rm -rf _site/ ../_site/
 jekyll build
 git add --all
 git commit -m "`date`"
 git push origin xphelp
-git subtree push --prefix _site/ origin master
+cp -R _site ..
+git checkout master
+cp -R ../_site/* .
+git add --all
+git commit -m "`date`"
+git push origin master

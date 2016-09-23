24b85be6a5845cacdb71fdd25f037c3714112b9c
Seung-yeop Jeon <syjeon.xmd@gmail.com>
2016-09-23 15:18:20 +0900

    2016년 9월 23일 금요일 15시 18분 20초 KST

---
+++ ja/zh_CN/publi.sh
@@ -0,0 +1,16 @@
+# publi.sh
+# change the branch names appropriately
+git checkout xphelp
+rm -rf _site/
+jekyll build
+git add --all
+git commit -m "`date`"
+git push origin xphelp
+cp -R _site ..
+git checkout master
+cp -R ../_site/* .
+git add --all
+git commit -m "`date`"
+git push origin master
+rm -rf ../_site/
+git checkout xphelp
